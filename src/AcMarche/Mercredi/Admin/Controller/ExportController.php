b23184938127fe977e3c3b23412a8da420722604
jfsenechal <jfsenechal@gmail.com>
2019-06-27 14:05:36 +0200

    add xls export

--- src/AcMarche/Mercredi/Admin/Controller/ExportController.php
+++ src/AcMarche/Mercredi/Admin/Controller/ExportController.php
@@ -3,17 +3,22 @@
 namespace AcMarche\Mercredi\Admin\Controller;
 
 use AcMarche\Mercredi\Admin\Entity\Enfant;
+use AcMarche\Mercredi\Admin\Repository\JourRepository;
+use AcMarche\Mercredi\Admin\Repository\PresenceRepository;
 use AcMarche\Mercredi\Admin\Service\PresenceService;
-use AcMarche\Mercredi\Plaine\Service\PlaineService;
+use AcMarche\Mercredi\Admin\Service\TuteurUtils;
 use AcMarche\Mercredi\Commun\Utils\DateService;
 use AcMarche\Mercredi\Commun\Utils\ScolaireService;
+use AcMarche\Mercredi\Plaine\Repository\PlaineJourRepository;
+use AcMarche\Mercredi\Plaine\Repository\PlainePresenceRepository;
+use AcMarche\Mercredi\Plaine\Service\PlaineService;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use Symfony\Component\HttpFoundation\StreamedResponse;
 
 /**
  * Class DefaultController
@@ -38,17 +43,53 @@ class ExportController extends AbstractController
      * @var ScolaireService
      */
     private $scolaireService;
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+    /**
+     * @var JourRepository
+     */
+    private $jourRepository;
+    /**
+     * @var PlaineJourRepository
+     */
+    private $plaineJourRepository;
+    /**
+     * @var PresenceRepository
+     */
+    private $presenceRepository;
+    /**
+     * @var PlainePresenceRepository
+     */
+    private $plainePresenceRepository;
+    /**
+     * @var TuteurUtils
+     */
+    private $tuteurUtils;
 
     public function __construct(
         DateService $dateService,
         PlaineService $plaineService,
         PresenceService $presenceService,
-        ScolaireService $scolaireService
+        ScolaireService $scolaireService,
+        SessionInterface $session,
+        JourRepository $jourRepository,
+        PlaineJourRepository $plaineJourRepository,
+        PresenceRepository $presenceRepository,
+        PlainePresenceRepository $plainePresenceRepository,
+        TuteurUtils $tuteurUtils
     ) {
         $this->plaineService = $plaineService;
         $this->dateService = $dateService;
         $this->presenceService = $presenceService;
         $this->scolaireService = $scolaireService;
+        $this->session = $session;
+        $this->jourRepository = $jourRepository;
+        $this->plaineJourRepository = $plaineJourRepository;
+        $this->presenceRepository = $presenceRepository;
+        $this->plainePresenceRepository = $plainePresenceRepository;
+        $this->tuteurUtils = $tuteurUtils;
     }
 
     /**
@@ -215,5 +256,74 @@ class ExportController extends AbstractController
             }
             $ligne++;
         }
+    }
+
+    /**
+     *
+     *
+     * @Route("/xls/presences/{id}/{type}", name="presence_xls", methods={"GET"})
+     * @IsGranted("ROLE_MERCREDI_ADMIN")
+     *
+     */
+    public function presenceXls(int $id, string $type)
+    {
+        $args = $this->session->get(MessageController::KEY_GROUP_SESSION, []);
+        if (count($args) < 1) {
+            return $this->redirectToRoute('presence');
+        }
+
+        if ($type === 'mercredi') {
+            $jour = $this->jourRepository->find($id);
+            $presences = $this->presenceRepository->findBy(['jour' => $jour]);
+        }
+
+        if ($type === 'plaine') {
+            $jour = $this->plaineJourRepository->find($id);
+            $presences = $this->plainePresenceRepository->findBy(['jour' => $jour]);
+        }
+
+        $enfants = [];
+        foreach ($presences as $presence) {
+            if ($type === 'plaine') {
+                $plaine_enfant = $presence->getPlaineEnfant();
+                $enfant = $plaine_enfant->getEnfant();
+            } else {
+                $enfant = $presence->getEnfant();
+            }
+            $enfants[] = $enfant;
+        }
+
+        $spreadsheet = new Spreadsheet();
+
+        $sheet = $spreadsheet->getActiveSheet();
+
+        $c = 1;
+        $sheet
+            ->setCellValue('A'.$c, 'Nom')
+            ->setCellValue('B'.$c, 'Prénom')
+            ->setCellValue('C'.$c, 'Né le');
+
+        $ligne = 3;
+        foreach ($enfants as $enfant) {
+            $colonne = "A";
+            $neLe = $enfant->getBirthday() ? $enfant->getBirthday()->format('d-m-Y') : "";
+            $sheet->setCellValue($colonne.$ligne, $enfant->getNom());
+            $colonne++;
+            $sheet->setCellValue($colonne.$ligne, $enfant->getPrenom());
+            $colonne++;
+            $sheet->setCellValue($colonne.$ligne, $neLe);
+            $ligne++;
+        }
+
+        $fileName = 'listing-presences.xls';
+
+        $writer = new Xlsx($spreadsheet);
+        $temp_file = tempnam(sys_get_temp_dir(), $fileName);
+
+        // Create the excel file in the tmp directory of the system
+        $writer->save($temp_file);
+
+        // Return the excel file as an attachment
+        return $this->file($temp_file, $fileName, ResponseHeaderBag::DISPOSITION_INLINE);
     }
 }
