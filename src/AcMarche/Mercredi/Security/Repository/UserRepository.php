9a36534795267ec9d8d4a016f9541d5403a1bfb5
jfsenechal <jfsenechal@gmail.com>
2020-08-26 10:30:24 +0200

    migrate sf 5.1

--- src/AcMarche/Mercredi/Security/Repository/UserRepository.php
+++ src/AcMarche/Mercredi/Security/Repository/UserRepository.php
@@ -7,17 +7,27 @@ use AcMarche\Mercredi\Security\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\ORM\NonUniqueResultException;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
  * @method User|null findOneBy(array $criteria, array $orderBy = null)
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class UserRepository extends ServiceEntityRepository
+class UserRepository extends ServiceEntityRepository implements UserLoaderInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
+    }
+
+    public function loadUserByUsername($usernameOrEmail): ?User
+    {
+        return $this->createQueryBuilder('user')
+            ->andWhere('user.email = :email OR user.username = :email')
+            ->setParameter('email', $usernameOrEmail)
+            ->getQuery()
+            ->getOneOrNullResult();
     }
 
     public function insert(User $user)
@@ -40,23 +50,6 @@ class UserRepository extends ServiceEntityRepository
     {
         $this->_em->remove($user);
         $this->save();
-    }
-
-    /**
-     * @param $username
-     *
-     * @return User
-     *
-     * @throws NonUniqueResultException
-     */
-    public function loadUserByUsername($username)
-    {
-        return $this->createQueryBuilder('u')
-            ->where('u.username = :username OR u.email = :email')
-            ->setParameter('username', $username)
-            ->setParameter('email', $username)
-            ->getQuery()
-            ->getOneOrNullResult();
     }
 
     public function search($args)
